6f142691a55bae336bd1e84b04fd6519d6fc3168
nesamouzehkesh <n.mouzehkesh@gmail.com>
2017-07-26 11:27:41 +1000

    toggle the state for two different views

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,49 @@ import React from 'react';
 
 import RecipeDetail from './RecipeDetail';
 import RecipeList from './RecipeList';
+import CreateForm from './CreateForm';
 
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showCreate: false
+        };
+
+        this.showCreate = this.showCreate.bind(this);
+    }
+
+    showCreate() {
+        this.setState({
+            showCreate: true
+        });
+}
+
     render() {
         return (
             <div className="container">
                 <h1>Recipe App</h1>
+
                 <div className="row">
                     <div className="col-xs-4">
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            style={{
+                                width: '100%',
+                                marginBottom: '5px'
+                            }}
+                            onClick={this.showCreate}
+                        >
+                            Create New Recipe
+                        </button>
                         <RecipeList/>
                     </div>
 
                     <div className="col-xs-8">
-                        <RecipeDetail/>
+                        {this.state.showCreate ? <CreateForm/> : <RecipeDetail/>}
                     </div>
                 </div>
             </div>
